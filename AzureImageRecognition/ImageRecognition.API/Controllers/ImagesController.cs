230fdce69e46804db93f8bfe933cde52b2dac403
Idris Lokhandwala <Idris.Lokhandwala@planonsoftware.com>
2020-01-24 23:40:10 +0100

    replaced frombody to fromform

--- AzureImageRecognition/ImageRecognition.API/Controllers/ImagesController.cs
+++ AzureImageRecognition/ImageRecognition.API/Controllers/ImagesController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using ImageRecognition.API.Common;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
@@ -25,44 +27,51 @@ namespace ImageRecognition.API.Controllers
         /// <summary>
         /// Retrieves tags by analyzing image content
         /// </summary>
-        /// <param name="base64Image">The base64 string representation of the image</param>
+        /// <param name="file">The image file sent through the form data</param>
         /// <returns>A list of tags</returns>
         [HttpPost("tags")]
-        public async Task<IActionResult> RetrieveTags([FromBody] string base64Image)
+        public async Task<IActionResult> RetrieveTags([FromForm] IFormFile file1 )
         {
 
-            var tags = new List<string>();
-            var client = ComputerVisionClientFactory.Authenticate(this.azureConfig.EndPoint, this.azureConfig.SubscriptionKey);
-            byte[] data = System.Convert.FromBase64String(base64Image);
-
-            using (var memoryStream = new MemoryStream(data))
+            try
             {
-                var results = await client.TagImageInStreamWithHttpMessagesAsync(memoryStream);
+                var tags = new List<string>();
+                var client = ComputerVisionClientFactory.Authenticate(this.azureConfig.EndPoint, this.azureConfig.SubscriptionKey);
+                var stream = new MemoryStream();
+                file1.CopyTo(stream);
+
+                var results = await client.TagImageInStreamWithHttpMessagesAsync(stream);
                 foreach (var tag in results.Body.Tags)
                 {
                     tags.Add(tag.Name);
                 }
-            }
 
-            return this.Ok(tags);
+                return this.Ok(tags);
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(500, ex);
+            }
         }
 
         /// <summary>
         /// Retrieves description information of the image
         /// </summary>
-        /// <param name="base64Image">The base64 string representation image</param>
+        /// <param name="file">The image file sent through the form data</param>
         /// <returns>A list of possible descriptions of the image</returns>
         [HttpPost("descriptions")]
-        public async Task<IActionResult> RetrieveDescription([FromBody] string base64Image)
+        public async Task<IActionResult> RetrieveDescription([FromForm] IFormFile file1)
         {
-            var client = ComputerVisionClientFactory.Authenticate(this.azureConfig.EndPoint, this.azureConfig.SubscriptionKey);
-            byte[] data = System.Convert.FromBase64String(base64Image);
 
-            var descriptions = new List<string>();
-
-            using (var memoryStream = new MemoryStream(data))
+            try
             {
-                var results = await client.DescribeImageInStreamWithHttpMessagesAsync(memoryStream);
+                var client = ComputerVisionClientFactory.Authenticate(this.azureConfig.EndPoint, this.azureConfig.SubscriptionKey);
+                var stream = new MemoryStream();
+                file1.CopyTo(stream);
+
+                var descriptions = new List<string>();
+
+                var results = await client.DescribeImageInStreamWithHttpMessagesAsync(stream);
                 foreach (var caption in results.Body.Captions)
                 {
                     descriptions.Add($"description:{caption.Text}, confidence level:{caption.Confidence}");
@@ -70,6 +79,12 @@ namespace ImageRecognition.API.Controllers
 
                 return this.Ok(descriptions);
             }
+            catch (Exception ex)
+            {
+
+                return this.StatusCode(500, ex);
+            }
+
         }
     }
 }
