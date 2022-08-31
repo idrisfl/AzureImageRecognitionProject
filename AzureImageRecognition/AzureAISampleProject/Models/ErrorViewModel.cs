0b0b9623b7a3fecf325f5876285d1bed7d213a8c
Idris LOKHANDWALA <ilokhandwala@microsoft.com>
2022-08-31 23:02:36 +0200

    remove unused project

--- AzureImageRecognition/AzureAISampleProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AzureAISampleProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
