edd97801780118cd2a82e1d8c4fa3a3766bfef40
Idris LOKHANDWALA <ilokhandwala@microsoft.com>
2022-09-10 16:51:44 +0200

    search - add baseline controller

--- AzureImageRecognition/ImageRecognition.UI/Controllers/ImageUploadController.cs
+++ AzureImageRecognition/ImageRecognition.UI/Controllers/ImageUploadController.cs
@@ -3,77 +3,75 @@ using ImageRecognition.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-namespace ImageRecognition.UI.Controllers
+namespace ImageRecognition.UI.Controllers;
+
+public class ImageUploadController : Controller
 {
-    public class ImageUploadController : Controller
+    private readonly HttpClient _httpClient;
+
+    public ImageUploadController(IConfiguration config)
     {
-        private readonly string _imageRecognitionApiBaseUrl;
-        private readonly HttpClient _httpClient;
+        var _imageRecognitionApiBaseUrl = config.GetValue<string>("ImageRecognitionAPIBaseUrl");
+        _httpClient = new HttpClient { BaseAddress = new Uri(_imageRecognitionApiBaseUrl)  };
+    }
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public ImageUploadController(IConfiguration config)
-        {
-            _imageRecognitionApiBaseUrl = config.GetValue<string>("ImageRecognitionAPIBaseUrl");
-            _httpClient = new HttpClient { BaseAddress = new Uri(_imageRecognitionApiBaseUrl)  };
-        }
-        
-        public IActionResult Index()
-        {
-            return View();
-        }
+    [HttpPost]
+    public async Task<IActionResult> Upload(List<IFormFile> files)
+    {
+        List<string> resultOriginal = new();
+        List<string> translatedResult = new();
 
-        [HttpPost]
-        public async Task<IActionResult> Upload(List<IFormFile> files)
+        try
         {
-            List<string> resultOriginal = new List<string>();
-            List<string> translatedResult = new List<string>();
+            List<ImageResult> imageResult = new();
 
-            try
+            foreach (var file in files.Where(f => f.Length > 0))
             {
-                var imageResult = new List<ImageResult>();
-
-                foreach (var file in files.Where(f => f.Length > 0))
+                using var ms = new MemoryStream();
+                await file.CopyToAsync(ms);
+                var byteArrayContent = ms.ToArray();
+                ByteArrayContent byteContent = new(byteArrayContent);
+                var multipartContent = new MultipartFormDataContent
                 {
-                    using var ms = new MemoryStream();
-                    await file.CopyToAsync(ms);
-                    var byteArrayContent = ms.ToArray();
-                    ByteArrayContent byteContent = new(byteArrayContent);
-                    var multipartContent = new MultipartFormDataContent
-                    {
-                        { byteContent, "filetest", "filename" }
-                    };
+                    { byteContent, "filetest", "filename" }
+                };
 
-                    var response = await _httpClient.PostAsync("/api/images/describe",  multipartContent);
-                    var responseContent = await response.Content.ReadAsStringAsync();
-                    resultOriginal.Add(responseContent);
+                var response = await _httpClient.PostAsync("/api/images/describe",  multipartContent);
+                var responseContent = await response.Content.ReadAsStringAsync();
+                resultOriginal.Add(responseContent);
 
-                    var content = new StringContent(JsonConvert.SerializeObject(responseContent), Encoding.UTF8, "application/json");
-                    var finalResponse = await _httpClient.PostAsync("/api/translation/translate", content);
-                    var finalResponseContent = await finalResponse.Content.ReadAsStringAsync();
-                    translatedResult.Add(finalResponseContent);
+                var content = new StringContent(JsonConvert.SerializeObject(responseContent), Encoding.UTF8, "application/json");
+                var finalResponse = await _httpClient.PostAsync("/api/translation/translate", content);
+                var finalResponseContent = await finalResponse.Content.ReadAsStringAsync();
+                translatedResult.Add(finalResponseContent);
 
-                    var result = ParseResultOnly<IEnumerable<TranslatedResult>>(finalResponseContent);
+                var result = ParseResultOnly<IEnumerable<TranslatedResult>>(finalResponseContent);
 
-                    var fileResult = new ImageResult
-                    {
-                        ImageDescription = result.First().Translations.First().Text,
-                        Base64ImageString = Convert.ToBase64String(byteArrayContent, Base64FormattingOptions.None)
-                    };
-                    imageResult.Add(fileResult);
-                }
-                return View("Result", imageResult);
+                var fileResult = new ImageResult
+                {
+                    ImageDescription = result.First().Translations.First().Text,
+                    Base64ImageString = Convert.ToBase64String(byteArrayContent, Base64FormattingOptions.None)
+                };
+                imageResult.Add(fileResult);
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return View("Index");
-            }
+            return View("Result", imageResult);
         }
-
-        private static T ParseResultOnly<T>(string content)
+        catch (Exception e)
         {
-            var result = JsonConvert.DeserializeObject<T>(content);
-
-            return result;
+            Console.WriteLine(e.Message);
+            return View("Index");
         }
+    }
+
+    private static T ParseResultOnly<T>(string content)
+    {
+        var result = JsonConvert.DeserializeObject<T>(content);
+
+        return result;
     }
 }
---
+++ AzureImageRecognition/ImageRecognition.UI/Controllers/SearchController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageRecognition.UI.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Search(List<IFormFile> files)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
