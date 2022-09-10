8524e3561ef2097e754710f2c7050d1527ac313c
Idris LOKHANDWALA <ilokhandwala@microsoft.com>
2022-09-10 17:15:45 +0200

    minor UI changes

--- AzureImageRecognition/ImageRecognition.UI/Controllers/SearchController.cs
+++ AzureImageRecognition/ImageRecognition.UI/Controllers/SearchController.cs
@@ -4,6 +4,11 @@ namespace ImageRecognition.UI.Controllers
 {
     public class SearchController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         [HttpPost]
         public async Task<IActionResult> Search(List<IFormFile> files)
         {
